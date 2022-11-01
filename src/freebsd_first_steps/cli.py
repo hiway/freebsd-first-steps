8cef30c5df44c8e96fe8ae847688fd28152cecd5
Harshad Sharma <harshad@sharma.io>
2022-11-01 11:45:34 +0530

    Split serve and ssh_install commands

--- src/freebsd_first_steps/cli.py
+++ src/freebsd_first_steps/cli.py
@@ -1,8 +1,21 @@
-import click 
 import subprocess
 
+import click
+from plumbum import SshMachine
 
-@click.command()
+
+@click.group()
 def main():
+    pass
+
+
+@main.command()
+def serve():
     """Run as streamlit app."""
     subprocess.run(["streamlit", "run", "src/freebsd_first_steps/Host.py"])
+
+
+@main.command()
+def ssh_install():
+    """Install FreeBSD on a remote machine."""
+    subprocess.run(["streamlit", "run", "src/freebsd_first_steps/remote/Remote_Install.py"])
